b28d713575047364d956730a8ce0ad9ac6a09d63
JawaharAmbedkar <jawahar7988@gmail.com>
2025-09-30 19:47:07 +0530

    Fix turbo.json schema for Vercel build

--- packages/db/client.ts
+++ packages/db/client.ts
@@ -1,7 +1,8 @@
-
-import { PrismaClient } from '@repo/db/generated/prisma' // or '@prisma/client'
+// packages/db/client.ts
+import { PrismaClient } from '@prisma/client'
 
 declare global {
+  // Prevent multiple instances of Prisma Client in development
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined
 }
