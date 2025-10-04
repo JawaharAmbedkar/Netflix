188be4f02f5f4a4cd782fb40735271501cf4caf9
JawaharAmbedkar <jawahar7988@gmail.com>
2025-10-04 22:04:22 +0530

    Rename next.config.js → next.config.cjs to fix ESM issue

--- apps/web/next.config.js
+++
@@ -1,7 +0,0 @@
-// apps/web/next.config.js
-const withTM = require('next-transpile-modules')(['@repo/db']);
-
-module.exports = withTM({
-  reactStrictMode: true,
-  experimental: { optimizeCss: false }, // optional
-});
